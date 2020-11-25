12646e31b4a0d8720e357c45f0e201e298377894
Baran Baygan <baran@rettermobile.com>
2020-11-25 16:52:02 +0300

    Some changes in example project

--- Example/RBS/ViewController.swift
+++ Example/RBS/ViewController.swift
@@ -11,9 +11,7 @@ import RBS
 
 class ViewController: UIViewController {
     
-    let testCustomToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiI3YjdlY2VjNzIxZDU0NjI5YmVkMWQzYjFhZWMyMTBlOCIsImlkZW50aXR5IjoicmJzLmJ1c2luZXNzdXNlcmF1dGgiLCJpYXQiOjE2MDYyNDI1NzMsImV4cCI6MTYxNjI0MjU3Mn0.Pu_FTXaOwxRq7mnWurF2V3VtyVscxnxN-33M3thTlxk"
-    
-    let rbs = RBS(config: RBSConfig(projectId: "7b7ecec721d54629bed1d3b1aec210e8"))
+    let rbs = RBS(config: RBSConfig(projectId: "933a51e1c87a9ccc181d21fca91c2aad"))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +24,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func signInWithCustomToken(_ sender: Any) {
-        rbs.authenticateWithCustomToken(testCustomToken)
-    }
+    
     @IBAction func testButtonTapped(_ sender: Any) {
         
         rbs.send(action: "rbs.basicauth.request.VALIDATE_OTP",
@@ -53,6 +49,39 @@ class ViewController: UIViewController {
     @IBAction func signoutTapped(_ sender: Any) {
         rbs.signOut()
     }
+    @IBAction func searchProducts(_ sender: Any) {
+        
+        rbs.send(action: "rbs.product.request.SEARCH",
+                 data: [
+                    "searchTerm": "dove"
+                 ],
+                 onSuccess: { result in
+                    print("Result: \(result)")
+                 },
+                 onError: { error in
+                    print("Error Result: \(error)")
+                 })
+
+    }
+    @IBAction func loginBusinessUser(_ sender: Any) {
+        rbs.send(action: "rbs.businessuserauth.request.LOGIN",
+                 data: [
+                    "email": "email@test.com",
+                    "password": "password"
+                 ],
+                 onSuccess: { result in
+                    print("Result: \(result)")
+                    
+                    if let serviceResponse = result.first as? [String:Any],
+                       let resp = serviceResponse["response"] as? [String:Any],
+                       let customToken = resp["customToken"] as? String {
+                        self.rbs.authenticateWithCustomToken(customToken)
+                    }
+                 },
+                 onError: { error in
+                    print("Error Result: \(error)")
+                 })
+    }
 }
 
 extension ViewController : RBSClientDelegate {
@@ -60,6 +89,3 @@ extension ViewController : RBSClientDelegate {
         print("RBS authStatusChanged to \(toStatus)")
     }
 }
-
-
-
