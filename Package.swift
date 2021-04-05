ee42643548083c4dc098111886020d0516da945a
Baran Baygan <baran@rettermobile.com>
2021-04-05 11:31:32 +0300

    min platform versions

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@
 import PackageDescription
 let package = Package(
     name: "RBS",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         .library(name: "RBS", targets: ["RBS"])
     ],
