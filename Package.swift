1a523e39d0de1bc1c5962a0541ccb47e6b2c9c48
mtuzer <tuzermert@gmail.com>
2021-07-02 12:46:33 +0300

    Fix TrustKit issue

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     targets: [
         .target(name: "RBS", dependencies: [
-            "Moya", "Alamofire", "ObjectMapper", "KeychainSwift", "JWTDecode"
+            "Moya", "Alamofire", "ObjectMapper", "KeychainSwift", "JWTDecode", "TrustKit"
         ], path: "RBS/Classes")
     ]
 )
